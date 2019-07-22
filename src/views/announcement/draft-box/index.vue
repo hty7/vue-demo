40c9364bbf69832497354f9f16a38566172754cc
YJ <1553937164@qq.com>
2019-07-22 17:38:35 +0800

    完善公告模块

--- src/views/announcement/draft-box/index.vue
+++ src/views/announcement/draft-box/index.vue
@@ -1,13 +1,141 @@
 <template>
-  <div>无</div>  
+  <div class="announcement-list">
+    <template v-for="(alkey, index) in draftList">
+      <div class="announcement-item" :key="alkey.id">
+        <div class="announcement-item-header">
+          <v-avatar size="36px">
+            <img :src="alkey.avatarUrl" alt="头像">
+          </v-avatar>
+          <span class="name">{{ alkey.fromName }}</span>
+          <p class="self-title">
+            {{ alkey.title }}
+          </p>
+          <div class="announcement-tool">
+            <v-btn flat icon color="primary">
+              <v-icon>edit</v-icon>
+            </v-btn>
+            <v-btn flat icon color="red" @click.stop="confirmDialog = true;activeItem=index">
+              <v-icon>delete</v-icon>
+            </v-btn>
+          </div>
+        </div>
+        <v-divider light></v-divider>
+        <div class="announcement-item-body">
+          <span>{{ alkey.conetent }}</span>
+        </div>
+      </div>
+    </template>
+    <div v-if="!draftList || !draftList.length" style="text-align: center;">
+      暂无数据
+    </div>
+    <v-dialog v-model="confirmDialog" max-width="290">
+      <v-card>
+        <v-card-title class="headline">
+          提示
+        </v-card-title>
+        <v-card-text>
+          确定要彻底删除该草稿？
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn flat color="primary" @click="confirmDialog = !confirmDialog">
+            取消
+          </v-btn>
+          <v-btn flat color="red" @click="delAnnouncements([activeItem])">
+            删除
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
 </template>
 
 <script>
 export default {
-
+  data: () => ({
+    draftList: [
+      {
+        id: 'd802381m1',
+        fromName: 'TOM',
+        avatarUrl: require(`@/assets/images/avatar/20140524012047988.png`),
+        title: 'new announcement1',
+        conetent: 'Became a searcher,wanting to find out who I was and what made me unique. My view of myself was changing. I wanted a solid base to start from. I started to resist3 pressure to act in ways that I didn’t like any more,and I was delighted by who I really was. I came to feel much more sure that no one can ever take my place.'
+      },
+      {
+        id: 'd802381m1',
+        fromName: 'JANE',
+        avatarUrl: require(`@/assets/images/avatar/20160203022630582.png`),
+        title: 'new announcement1',
+        conetent: `Just as you need air to breathe, you need opportunity to succeed. It takes more than just breathing in the fresh air of opportunity, however. You must make use of that opportunity. That's not up to the opportunity. That's up to you. It doesn't matter what "floor" the opportunity is on. What matters is what you do with it.`
+      }
+    ],
+    activeItem: null,
+    confirmDialog: false
+  }),
+  methods: {
+    delAnnouncements (el=[]) {
+      if (el.length) {
+        el.forEach(e => {
+          this.draftList.splice(e,1)
+        })
+      }
+      this.confirmDialog = false
+    }
+  },
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+.announcement-list {
+  .announcement-item {
+    position: relative;
+    margin-bottom: 12px;
+    background: #fff;
+    padding: 8px 12px;
+    border-radius: 5px;
+    box-shadow: 0 3px 5px 1px rgba(181, 181, 181, 0.4);
+    transition: all .4s;
+    cursor: pointer;
+    &:hover {
+      box-shadow: 0 0px 7px 1px rgba(161, 161, 161, 0.8);
+      .announcement-item-header {
+        font-weight: bolder;
+      }
+      .announcement-item-body {
+        color: #000;
+      }
+    }
+    &-header {
+      position: relative;
+      margin-bottom: 8px;
+      .name {
+        margin-left: 12px;
+        font-size: 14px;
+      }
+      .self-title {
+        font-size: 15px;
+        font-weight: bold;
+        margin-bottom: 0;
+        margin-top: 3px;
+      }
+      .announcement-tool {
+        position: absolute;
+        top: 0;
+        right: 0;
+      }
+    }
+    &-body {
+      color: #797979;
+      padding: 5px 0;
+      span {
+        overflow: hidden;
+        display: -webkit-box;
+        text-overflow: ellipsis;
+        -webkit-line-clamp: 2;
+        -webkit-box-orient: vertical;
+        white-space: normal;
+      }
+    }
+  }
+}
 </style>
--- src/views/announcement/edit/index.vue
+++ src/views/announcement/edit/index.vue
@@ -1,45 +1,56 @@
 <template>
-  <div>
-    <v-container fluid>
-      <v-layout justify-center row wrap>
-        <v-flex xs12 md6 lg6>
-          <v-form ref="form" lazy-validation>
-            <v-text-field
-              v-model="announcementMsg.title"
-              label="标题"
-              required
-            ></v-text-field>
-            <v-combobox
-              v-model="announcementMsg.acceptUsers"
-              :items="items"
-              label="公告接受人员"
-              multiple
-              chips
-            >
-              <template v-slot:selection="data">
-                <v-chip
-                  :key="JSON.stringify(data.item)"
-                  :selected="data.selected"
-                  :disabled="data.disabled"
-                  class="v-chip--select-multi"
-                  @input="data.parent.selectItem(data.item)"
-                >
-                  <v-avatar
-                    class="accent white--text"
-                    v-text="data.item.slice(0, 1).toUpperCase()"
-                  ></v-avatar>
-                  {{ data.item }}
-                </v-chip>
-              </template>
-            </v-combobox>
-          </v-form>
-        </v-flex>
-      </v-layout>
-    </v-container>
+  <div class="announcement-content">
+    <v-layout justify-center row wrap>
+      <v-flex xs12 md8 lg8>
+        <v-form ref="form" lazy-validation>
+          <v-text-field
+            v-model="announcementMsg.title"
+            label="标题"
+            required
+          ></v-text-field>
+          <v-combobox
+            v-model="announcementMsg.acceptUsers"
+            :items="items"
+            label="公告接受人员"
+            multiple
+            chips
+          >
+            <template v-slot:selection="data">
+              <v-chip
+                small
+                :key="JSON.stringify(data.item)"
+                :selected="data.selected"
+                :disabled="data.disabled"
+                class="v-chip--select-multi"
+                @input="data.parent.selectItem(data.item)"
+              >
+                <v-avatar
+                  class="accent white--text"
+                  v-text="data.item.slice(0, 1).toUpperCase()"
+                ></v-avatar>
+                {{ data.item }}
+              </v-chip>
+            </template>
+          </v-combobox>
+          <div id="editor"></div>
+          <div class="upload-container">
+            <p>附件：</p>
+            <div class="upload-container-dragger">
+              <img src="@/assets/images/icon-upload.png" alt="upload" class="upload-img">
+              <div class="upload__text">
+                将文件拖到此处，或点击上传
+              </div>
+            </div>
+          </div>
+        </v-form>
+      </v-flex>
+    </v-layout>
   </div>  
 </template>
 
 <script>
+import 'quill/dist/quill.snow.css'
+import * as Quill from 'quill'
 export default {
   data: () => ({
     announcementMsg: {
@@ -51,11 +62,70 @@ export default {
       'Jane',
       'Tone',
       'Buteng'
+    ],
+    editorDom: null,
+    toolbarOptions: [
+      ['bold', 'italic', 'underline', 'strike'], //开关按钮
+      ['blockquote', 'code-block'],
+      [{'list': 'ordered'}, {'list': 'bullet'}],
+      [{'script': 'sub'}, {'script': 'super'}], // 上标/下标
+      [{'indent': '-1'}, {'indent': '+1'}], // 减少缩进/缩进
+      [{'header': [1, 2, 3, 4, 5, 6, false]}],
+      [{'color': []}, {'background': []}], //使用主题的默认下拉
+      [{'align': []}],
+      ['clean'] //移除格式化
     ]
-  })
+  }),
+  mounted() {
+    this.$nextTick(() => {
+      this.editorDom = new Quill('#editor', {
+        modules: {
+          toolbar: this.toolbarOptions
+        },
+        theme: 'snow',
+        placeholder:'请输入',
+      })
+    })
+  },
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+#editor {
+  min-height: 120px;
+}
+.announcement-content {
+  position: relative;
+  margin-bottom: 12px;
+  background: #fff;
+  padding: 48px 12px;
+  border-radius: 5px;
+  box-shadow: 0 3px 5px 1px rgba(181, 181, 181, 0.4);
+  .upload-container {
+    margin-top: 12px;
+    &-dragger {
+      background-color: #fff;
+      border: 1px dashed #d9d9d9;
+      border-radius: 6px;
+      box-sizing: border-box;
+      width: 100%;
+      height: 180px;
+      text-align: center;
+      cursor: pointer;
+      position: relative;
+      overflow: hidden;
+      .upload-img {
+        width: 64px;
+        margin: 40px 0 16px;
+        line-height: 50px;
+      }
+      .upload__text {
+        color: #606266;
+        font-size: 14px;
+        text-align: center;
+      }
+    }
+  }
+}
 </style>
+
